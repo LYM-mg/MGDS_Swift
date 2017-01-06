9b2624e1fcdc07a40b3fa0c06a2ad8f2cf20b66b
i-Techsys.com <1292043630@qq.com>
2017-01-06 22:18:50 +0800

    友盟登录之微博登录

---
+++ MGDS_Swift/MGDS_Swift/Class/Login/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  MGDS_Swift
+//
+//  Created by i-Techsys.com on 17/1/6.
+//  Copyright © 2017年 i-Techsys. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
